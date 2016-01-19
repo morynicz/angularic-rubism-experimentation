e410344e441378e71555121603a701725210456e
Michał Orynicz <morynicz@gmail.com>
2016-01-19 21:52:44 +0100

    Make templates work

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,15 @@
-angular.module('receta',[]);
+angular.module('receta',['templates', 'ngRoute', 'controllers']);
+
+angular.module('receta').config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/',{
+	templateUrl: '_index.html',
+	controller: 'RecipesController'
+    });
+}]);
+
+angular.module('controllers',[]);
+
+angular.module('controllers').controller('RecipeController',[ '$scope', function($scope){
+}]);
+				
+			
