3f4a211fb4f481b99c9c5099bdd163bf99e217c4
Michał Orynicz <morynicz@gmail.com>
2016-01-20 08:17:24 +0100

    First commit with everything up until now

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a testing ground project for uning Rails in the back and Angular.js onthe front end of a single project.
-Findings shall be used for rewriting my other project: tournament-service
+# todoapp
 
-# angularic-rubism-experimentation
+a [Sails](http://sailsjs.org) application
+
---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('todoApp', ['ngRoute', 'ui.bootstrap']);
+
+angular.module('todoApp').config(['$routeProvider',
+	function($routeProvider) {
+	    $routeProvider.when('/', {
+		templateUrl: '/templates/_todo.html',
+		controllers: 'TodoCtrl as todo'
+	    }).otherwise({
+		redirectTo: '/',
+		caseInsensitiveMatch: true
+	    })
+	}])
+
+angular.module('todoApp').controller('TodoCtrl, ['$scope, '$rootScope', 'TodoService',
+				     function($scope, $rootScope, TodoService){
+
+    $scope.formData = {};
+    $scope.todos = [];
+
+    TodoService.getTodos().then(function(response){
+	$scope.todos = response;
+    });
+
+    $scope.addTodo = function() {
+	TodoService.addTodo($scope.formData).then(function(response){
+	    $scope.todos.push($scope.formData);
+	    $scope.formData = {};
+	});
+    }
+
+    $scope.removeTodo = function(todo) {
+	TodoService.removeTodo(todo).then(function(response) {
+	    $scope.todos.splice($scope.todos.indexOf(todo), 1);
+	});
+    }
+}]);
