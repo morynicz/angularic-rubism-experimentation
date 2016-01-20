3d6ebf0bc675ca89cfbb7f382bd4001e97446230
Michał Orynicz <morynicz@gmail.com>
2016-01-20 07:12:02 +0100

    Add table of fake data

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,9 +7,28 @@ angular.module('receta').config(['$routeProvider', function($routeProvider) {
     });
 }]);
 
+recipes = [
+    {
+	id: 1,
+	name: 'Baked Potato w/ Cheese'
+    },
+    {
+	id: 2,
+	name: 'Garlic Mashed Potatoes'
+    },
+    {
+	id: 3,
+	name: 'Potatoes Au Gartin'
+    },
+    {
+	id: 4,
+	name: 'Baked Brussel Sprouts'
+    }];
+
 angular.module('controllers',[]);
 
 angular.module('controllers').controller('RecipeController',[ '$scope', function($scope){
+    
 }]);
 				
 			
