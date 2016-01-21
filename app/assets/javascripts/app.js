c1150ccb9b008678a3e5d5ce93671c752f5624e7
Michał Orynicz <morynicz@gmail.com>
2016-01-21 22:13:27 +0100

    Fix typo and style in app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,48 +1,46 @@
 angular.module('receta',['templates', 'ngRoute', 'controllers']);
 
 angular.module('receta').config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/',{
-	templateUrl: '_index.html',
-	controller: 'RecipesController'
-    });
+  $routeProvider.when('/',{
+    templateUrl: '_index.html',
+    controller: 'RecipesController'
+  });
 }]);
 
 var recipes = [
-    {
-	id: 1,
-	name: 'Baked Potato w/ Cheese'
-    },
-    {
-	id: 2,
-	name: 'Garlic Mashed Potatoes'
-    },
-    {
-	id: 3,
-	name: 'Potatoes Au Gartin'
-    },
-    {
-	id: 4,
-	name: 'Baked Brussel Sprouts'
-    }];
+  {
+    id: 1,
+    name: 'Baked Potato w/ Cheese'
+  },
+  {
+    id: 2,
+    name: 'Garlic Mashed Potatoes'
+  },
+  {
+    id: 3,
+    name: 'Potatoes Au Gartin'
+  },
+  {
+    id: 4,
+    name: 'Baked Brussel Sprouts'
+  }];
 
-angular.module('controllers',[]);
+  angular.module('controllers',[]);
 
-angular.module('controllers').controller('RecipeController',[ '$scope', '$routeParams', '$location',
-							      function($scope, $routeParams, $location) {
-								  var keywords;
-								  $scope.search = function(keywords) {
-								      $location.path("/").search('keywords',keywords);
-								  }
+  angular.module('controllers').controller('RecipesController',[ '$scope', '$routeParams', '$location',
+  function($scope, $routeParams, $location) {
+    var keywords;
+    $scope.search = function(keywords) {
+      $location.path("/").search('keywords',keywords);
+    }
 
-								  if ($routeParams.keywords) {
-								      keywords = $routeParams.keywords.toLowerCase();
-								      $scope.recipes = recipes.filter(function(recipe) {
-									  return recipe.name.toLowerCase().indexOf(keywords) !== -1
-								      });
-								  } else {
-								      $scope.recipes = [];
-								  }
-												
-}]);
-				
-			
+    if ($routeParams.keywords) {
+      keywords = $routeParams.keywords.toLowerCase();
+      $scope.recipes = recipes.filter(function(recipe) {
+        return recipe.name.toLowerCase().indexOf(keywords) !== -1
+      });
+    } else {
+      $scope.recipes = [];
+    }
+
+  }]);
