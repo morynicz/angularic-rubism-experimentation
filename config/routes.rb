29b315be93d396a199974bc3400bff16417ea8e0
Michał Orynicz <morynicz@gmail.com>
2016-01-23 14:50:06 +0100

    Add route for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  #get 'recipes/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  resources :recipes, only: [:index]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
