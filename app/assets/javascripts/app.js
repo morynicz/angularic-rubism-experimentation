5d71d1d5e9f51176bec69380173d49a97ed4ec79
Michał Orynicz <morynicz@gmail.com>
2016-01-30 13:02:35 +0100

    Move RecipesController to its own file

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -16,27 +16,3 @@ angular.module('receta').config([
 ]);
 
 angular.module('controllers',['ngResource']);
-
-angular.module('controllers').controller('RecipesController',[
-  '$scope',
-  '$routeParams',
-  '$location',
-  '$resource',
-  function($scope, $routeParams, $location, $resource) {
-    var recipe;
-    $scope.search = function(keywords) {
-      $location.path("/").search('keywords',keywords);
-    }
-
-    recipe = $resource('/recipes/:recipeId', {recipeId: "@id", format: "json"});
-
-    if ($routeParams.keywords) {
-      return recipe.query({ keywords: $routeParams.keywords}, function(results){
-        return $scope.recipes = results;
-      });
-    } else {
-      $scope.recipes = [];
-    }
-
-  }
-]);
